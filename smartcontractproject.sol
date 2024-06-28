ce409274f112ca8ba8725fca09128284eba80559
AjayGodara2345 <godaraajay2345@gmail.com>
2024-06-28 15:39:22 +0530

    Update smartcontractproject.sol

--- smartcontractproject.sol
+++ smartcontractproject.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ErrorHandlingExample {
+    uint256 public num;
+
+    function setNum(uint256 _num) public {
+        // Require that _num is greater than 0
+        require(_num > 0, "Number must be greater than zero");
+
+        // Set the value
+        num = _num;
+    }
+
+    function assertExample() public pure returns (bool) {
+        uint256 x = 10;
+        uint256 y = 5;
+
+        // Assert that x is greater than y
+        assert(x > y);
+
+        // This will never be reached if the assertion passes
+        return true;
+    }
+
+    function revertExample(uint256 _amount) public pure returns (uint256) {
+        // Example function using revert
+        if (_amount <= 100) {
+            revert("Amount must be greater than 100");
+        }
+
+        return _amount * 2;
+    }
+}
 
